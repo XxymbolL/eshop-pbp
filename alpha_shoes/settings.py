cd1d9f7dfe810623c72278c5d657ac0cee5fb0d5
XxymbolL <rifqy.Kurniawan1703@gmail.com>
2025-09-05 21:53:00 +0700

    update fix

--- alpha_shoes/settings.py
+++ alpha_shoes/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET')
+SECRET_KEY = 'django-insecure-h+&=6u#la&70lr8wu&_x18dbhlfe^9$g2-qcd9vmu5dx=_x13t'
 
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
